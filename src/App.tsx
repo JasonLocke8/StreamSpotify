3a24c2066fb8515da5547bdedae0085970650f81
Nico Gonzalez <nicogonzalez2000@gmail.com>
2024-12-29 14:48:36 -0300

    Actualizacion app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import NowPlaying from './components/NowPlaying';
 
 function App() {
-  useEffect(() => {
-    const hash = window.location.hash;
-    if (hash) {
-      const token = new URLSearchParams(hash.substring(1)).get('access_token');
-      if (token) {
-        window.location.href = `https://jasonlocke8.github.io/StreamSpotify?access_token=${token}`;
-      }
-    }
-  }, []);
-
   return (
     <div className="min-h-screen flex items-center justify-center">
       <NowPlaying />
